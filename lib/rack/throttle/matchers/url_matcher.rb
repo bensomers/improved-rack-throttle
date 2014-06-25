0ec87600f2735dbd1ccf1b86f8fff08b1c52875c
Ben Somers <somers.ben@gmail.com>
2014-06-25 11:38:17 -0700

    fix typo in comment on UrlMatcher

--- lib/rack/throttle/matchers/url_matcher.rb
+++ lib/rack/throttle/matchers/url_matcher.rb
@@ -4,7 +4,7 @@ module Rack; module Throttle
   # requested. For instance, you may care about limiting requests
   # to a machine-consumed API, but not be concerned about requests
   # coming from browsers.
-  # UrlMatchers take Regexp object to matcha gainst the request path.
+  # UrlMatchers take Regexp object to match against the request path.
   class UrlMatcher < Matcher
     ###
     # @param [Rack::Request] request
